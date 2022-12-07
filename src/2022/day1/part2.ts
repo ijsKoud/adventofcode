b972e9177e8560e576ff18e29d7a50a1abb5664b
ijsKoud <github@ijskoud.dev>
2022-12-07 18:33:18 +0100

    chore: add day1 data

--- src/2022/day1/part2.ts
+++ src/2022/day1/part2.ts
@@ -4,7 +4,7 @@ import { srcDir } from "../../lib/constants.js";
 import { bold } from "colorette";
 
 async function run() {
-	const dataFile = join(srcDir, "..", "data", "2022", "day1", "part1.txt");
+	const dataFile = join(srcDir, "..", "data", "2022", "day1", "calories.txt");
 	const file = await readFile(dataFile, { encoding: "utf8" });
 
 	const splitPerElf = file.split("\n\n");
