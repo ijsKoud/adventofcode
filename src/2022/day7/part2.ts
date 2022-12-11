33de8a7227a61be88d082ef60bbc7ae5b05f0beb
ijsKoud <github@ijskoud.dev>
2022-12-11 20:32:12 +0100

    feat(Day7): add part 2

---
+++ src/2022/day7/part2.ts
@@ -0,0 +1,118 @@
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { srcDir } from "../../lib/constants.js";
+import { getProperty, setProperty, deepKeys } from "dot-prop";
+import { bold } from "colorette";
+
+function getFileSystem(output: string) {
+	let currentDir: string | undefined;
+	let structure: FileStructure = { _files: 0 };
+	const lines = output.split("\n");
+
+	const updateCurrentDir = (p3: string) => {
+		switch (p3) {
+			case "..":
+				{
+					const dirs = currentDir ? currentDir.replaceAll(".", "/") : "/";
+					const newDir = join(dirs, "..").replaceAll("/", ".");
+					currentDir = newDir === "." ? undefined : newDir;
+				}
+				break;
+			case "/":
+				currentDir = undefined;
+				break;
+			default:
+				currentDir = currentDir ? `${currentDir}.${p3}` : p3;
+				break;
+		}
+	};
+
+	const addDir = (p2: string) => {
+		if (currentDir) {
+			const existingData = getProperty(structure, currentDir) as FileStructure;
+			setProperty(structure, currentDir, { ...existingData, [p2]: { _files: 0 } });
+		} else structure = { ...structure, [p2]: { _files: 0 } };
+	};
+
+	const addFile = (p1: string) => {
+		if (currentDir) {
+			const dir = `${currentDir}._files`;
+			const size = getProperty(structure, dir, 0) as number;
+			setProperty(structure, dir, size + Number(p1));
+		} else structure._files += Number(p1);
+	};
+
+	for (const line of lines) {
+		const [p1, p2, p3] = line.split(" ");
+		if (p1 === "$" && p2 === "cd") updateCurrentDir(p3);
+		else if (p1 === "dir") addDir(p2);
+		else if (!isNaN(Number(p1))) addFile(p1);
+	}
+
+	return structure;
+}
+
+function getTotalSize(fileSystem: FileStructure) {
+	const sizeTable: SizeTable = {};
+	const dKeys = deepKeys(fileSystem).map((key) => key.replace("._files", ""));
+	const deepToClose = dKeys.sort((a, b) => b.split(".").length - a.split(".").length);
+
+	for (const key of deepToClose) {
+		if (key === "_files") continue;
+
+		const content = getProperty(fileSystem, key);
+		const keys = Object.keys(content);
+		let size = (content as FileStructure)._files;
+
+		for (const k of keys) {
+			if (k === "_files") continue;
+			const vSize = sizeTable[`${key}.${k}`];
+
+			size += vSize;
+		}
+
+		sizeTable[key] = size;
+	}
+
+	const keys = Object.keys(sizeTable);
+	let size = fileSystem._files;
+
+	for (const k of keys) {
+		if (k.includes(".")) continue;
+		size += sizeTable[k];
+	}
+
+	sizeTable[""] = size;
+
+	return sizeTable;
+}
+
+async function run() {
+	const dataFile = join(srcDir, "..", "data", "2022", "day7", "terminal.txt");
+	const file = await readFile(dataFile, { encoding: "utf8" });
+
+	const cleanLines = file.replaceAll("$ ls\n", "");
+	const fileSystem = getFileSystem(cleanLines);
+	const sizeTable = getTotalSize(fileSystem);
+
+	const sizes = Object.keys(sizeTable)
+		.map((key) => sizeTable[key])
+		.sort((a, b) => b - a);
+
+	const sizeUnused = 7e7 - sizes[0];
+	const sizeNeeded = 3e7 - sizeUnused;
+	const valid = sizes.filter((size) => size >= sizeNeeded).sort((a, b) => a - b);
+
+	console.log(`The directory that should be deleted has a size of ${bold(valid[0])}.`);
+}
+
+void run();
+
+interface FileStructure {
+	_files: number;
+	[key: string]: number | FileStructure;
+}
+
+interface SizeTable {
+	[dir: string]: number;
+}
